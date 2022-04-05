fe4e92e057b1f6a5b840fe994c090e36d7966915
Marianagiacconi <marianagiacconisevilla@gmail.com>
2022-04-05 00:55:40 -0300

    cambios

--- procesos[fork]/fork.py
+++ procesos[fork]/fork.py
@@ -1,10 +1,9 @@
 import argparse
 import os 
+
 parser = argparse.ArgumentParser()
-
-
-n=parser.add_argument('-n','--numero',type=int,help='int',action='store')
-v=parser.add_argument('-help',type=str, help ='python3 fork.py -n num or -v num')
+parser.add_argument('-n','--numero',type=int,help='int',action='store')
+parser.add_argument('-help',type=str, help ='python3 fork.py -n num or -v num')
 parser.add_argument('-v','--verbose',type=int, help= 'modo verbose',action='store')
 argsn = parser.parse_args
 argsv= parser.parse_args
