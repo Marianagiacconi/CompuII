f6a4ce84dcfff9ef35678534fd01d7480b5d5586
Marianagiacconi <marianagiacconisevilla@gmail.com>
2022-04-05 09:07:00 -0300

    fix

--- procesos[fork]/fork.py
+++ procesos[fork]/fork.py
@@ -1,23 +1,22 @@
 import argparse
 import os 
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-n','--numero',type=int,help='int',action='store')
-parser.add_argument('-help',type=str, help ='python3 fork.py -n num or -v num')
-parser.add_argument('-v','--verbose',type=int, help= 'modo verbose',action='store')
-argsn = parser.parse_args
-argsv= parser.parse_args
-
-
 def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-n',type=int,help='int',action='store')
+    parser.add_argument('--h',type=str, help ='python3 fork.py -n num or -v num')
+    parser.add_argument('-v',type=int, help= 'modo verbose',action='store')
+    args = parser.parse_args()
     #modo num
+    if args.n:
         n= os.fork()
-        print(f"PID=", os.getpid())
-        print(f"PPID=", os.getppid())
+        print(f"\n", os.getpid(), '-', os.getppid())
+
+    if args.v:
         #modo verbose    
         v= os.fork()
-        print(f"Inicio proceso hijo. ",os.getpid ())
-        print(f"Inicio proceso padre. ",os.getppid ())
+        print(f"\n Inicio proceso hijo. ",os.getpid ())
+        print(f"\n Inicio proceso padre. ",os.getppid ())
     
 
 
